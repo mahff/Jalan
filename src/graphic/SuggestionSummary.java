51e1c78805909d9a54343282bed8cbdc86b2ccbe
Anlodar <35761452+Anlodar@users.noreply.github.com>
2018-03-26 00:05:26 +0200

    Add files via upload

--- src/graphic/SuggestionSummary.java
+++ src/graphic/SuggestionSummary.java
@@ -1,7 +1,11 @@
 package graphic;
 
 import java.awt.Component;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 import java.io.File;
@@ -12,12 +16,17 @@ import javax.swing.ActionMap;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JSplitPane;
+import javax.swing.JViewport;
+import javax.swing.SwingUtilities;
 import javax.xml.stream.XMLStreamException;
 
 import org.apache.batik.swing.JSVGCanvas;
 import org.apache.batik.swing.JSVGScrollPane;
 
-import parsing.ShapeWays;
+import mapping.ShapeWays;
+import mapping.FirstZoomLevel;
+import mapping.SecondZoomLevel;
+import mapping.ThirdZoomLevel;
 
 public class SuggestionSummary {
 
@@ -25,7 +34,7 @@ public class SuggestionSummary {
 	static JComboBox<String> suggestion = new JComboBox<String>();
 	static JLabel summary = new JLabel();
 	static int zoomlevel = 3;
-
+	
 	public static Component SuggestionSummaryFrame() {
 
 		try {
@@ -36,33 +45,70 @@ public class SuggestionSummary {
 
 		final JSVGCanvas image = new JSVGCanvas();
 		image.setURI(new File("singapore.svg").toURI().toString());
-
+		image.setAutoscrolls(true);
 		JSVGScrollPane map = new JSVGScrollPane(image);
-		image.addMouseWheelListener(new MouseWheelListener() {
+			
+			 MouseAdapter adapter = new MouseAdapter() {
 
-			public void mouseWheelMoved(MouseWheelEvent evt) {
+                 private Point origin;
 
-				double rotation = evt.getPreciseWheelRotation();
-				ActionMap map = image.getActionMap();
-				Action action = null;
+                 public void mousePressed(MouseEvent e) {
+                     origin = new Point(e.getPoint());
+                 }
 
-				zoomlevel += rotation;
+                 public void mouseReleased(MouseEvent e) {
+                 }
 
-				if (zoomlevel > 0 && zoomlevel < 4) {
-					if (rotation > 0.0) {
-						action = map.get(JSVGCanvas.ZOOM_OUT_ACTION);
-					} else {
-						action = map.get(JSVGCanvas.ZOOM_IN_ACTION);
-					}
-					action.actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, null));
-				} else if (zoomlevel <= 0) {
-					setZoomlevel(1);
-				} else {
-					setZoomlevel(3);
-				}
-			}
-		});
+                 public void mouseDragged(MouseEvent e) {
+                     if (origin != null) {
+                         JViewport viewPort = (JViewport) SwingUtilities.getAncestorOfClass(JViewport.class, map);
+                         if (viewPort != null) {
+                             int deltaX = origin.x - e.getX();
+                             int deltaY = origin.y - e.getY();
 
+                             Rectangle view = viewPort.getViewRect();
+                             view.x += deltaX;
+                             view.y += deltaY;
+
+                             map.scrollRectToVisible(view);
+                         }
+                     }
+                 }
+                 
+                 
+                 public void mouseWheelMoved(MouseWheelEvent e) {
+
+     				double rotation = e.getPreciseWheelRotation();
+     				ActionMap map = image.getActionMap();
+     				Action action = null;
+
+     				zoomlevel += rotation;
+
+     				if (zoomlevel > 0 && zoomlevel < 4) {
+
+     					if (zoomlevel == 3) {
+     						new FirstZoomLevel();
+     						image.setURI(new File("singapore.svg").toURI().toString());
+     					} else if (zoomlevel == 2) {
+     						new SecondZoomLevel();
+     						image.setURI(new File("singapore.svg").toURI().toString());
+     					} else if (zoomlevel == 1) {
+     						new ThirdZoomLevel();
+     						image.setURI(new File("singapore.svg").toURI().toString());
+     					}
+     					action.actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, null));
+     				} else if (zoomlevel <= 0) {
+     					setZoomlevel(1);
+     				} else {
+     					setZoomlevel(3);
+     				}
+     			}
+			 };
+
+		map.addMouseListener(adapter);
+        map.addMouseMotionListener(adapter);
+        image.addMouseWheelListener(adapter);
+        
 		suggestion.addItem("Quickest");
 		suggestion.addItem("Cheapest");
 		suggestion.addItem("Touristic");
