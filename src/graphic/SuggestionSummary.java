a9a058535240022d6268b14d8facfeeccdd38332
mahff <33895997+mahff@users.noreply.github.com>
2018-03-31 16:44:07 +0300

    Drag is working well, zoom too but twice together

--- src/graphic/SuggestionSummary.java
+++ src/graphic/SuggestionSummary.java
@@ -2,133 +2,84 @@ package graphic;
 
 import java.awt.Component;
 import java.awt.Point;
-import java.awt.Rectangle;
-import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
-import java.awt.event.MouseWheelListener;
+import java.awt.geom.AffineTransform;
 import java.io.File;
-import java.io.IOException;
-
-import javax.swing.Action;
-import javax.swing.ActionMap;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JSplitPane;
-import javax.swing.JViewport;
-import javax.swing.SwingUtilities;
-import javax.xml.stream.XMLStreamException;
-
 import org.apache.batik.swing.JSVGCanvas;
-import org.apache.batik.swing.JSVGScrollPane;
-
-import mapping.ShapeWays;
-import mapping.FirstZoomLevel;
-import mapping.SecondZoomLevel;
-import mapping.ThirdZoomLevel;
 
 public class SuggestionSummary {
-
+	AffineTransform at;
+	double rapechelle;
 	static JSVGCanvas image = new JSVGCanvas();
 	static JComboBox<String> suggestion = new JComboBox<String>();
 	static JLabel summary = new JLabel();
-	static int zoomlevel = 3;
-	
-	public static Component SuggestionSummaryFrame() {
 
-		try {
-			ShapeWays.generateMap();
-		} catch (XMLStreamException | IOException e) {
-			e.printStackTrace();
-		}
+	public static Component SuggestionSummaryFrame() {
 
 		final JSVGCanvas image = new JSVGCanvas();
 		image.setURI(new File("singapore.svg").toURI().toString());
 		image.setAutoscrolls(true);
-		JSVGScrollPane map = new JSVGScrollPane(image);
-			
-			 MouseAdapter adapter = new MouseAdapter() {
+		image.addMouseWheelListener(new MouseAdapter() {
 
-                 private Point origin;
+			private double zoom = 1.0;
+			public static final double SCALE_STEP = 0.1d;
 
-                 public void mousePressed(MouseEvent e) {
-                     origin = new Point(e.getPoint());
-                 }
+			@Override
+			public void mouseWheelMoved(MouseWheelEvent e) {
+				double zoomFactor = -SCALE_STEP * e.getPreciseWheelRotation() * zoom;
+				zoom = Math.abs(zoom + zoomFactor);
+				AffineTransform at = new AffineTransform();
+				at.scale(zoom, zoom);
+				image.setRenderingTransform(at, true);
+				System.out.println("Zooom");
+			}
 
-                 public void mouseReleased(MouseEvent e) {
-                 }
+		});
 
-                 public void mouseDragged(MouseEvent e) {
-                     if (origin != null) {
-                         JViewport viewPort = (JViewport) SwingUtilities.getAncestorOfClass(JViewport.class, map);
-                         if (viewPort != null) {
-                             int deltaX = origin.x - e.getX();
-                             int deltaY = origin.y - e.getY();
+		MouseAdapter adapter = new MouseAdapter() {
 
-                             Rectangle view = viewPort.getViewRect();
-                             view.x += deltaX;
-                             view.y += deltaY;
+			private Point origin;
 
-                             map.scrollRectToVisible(view);
-                         }
-                     }
-                 }
-                 
-                 
-                 public void mouseWheelMoved(MouseWheelEvent e) {
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				origin = new Point(e.getPoint());
+			}
 
-     				double rotation = e.getPreciseWheelRotation();
-     				ActionMap map = image.getActionMap();
-     				Action action = null;
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				AffineTransform at = new AffineTransform();
+				int deltaX = origin.x - e.getX();
+				int deltaY = origin.y - e.getY();
+				image.setEnableImageZoomInteractor(true);
+				at.translate(deltaX, deltaY);
+				image.setRenderingTransform(at, true);
+				System.out.println(deltaX);
+				System.out.println(deltaY);
 
-     				zoomlevel += rotation;
+			}
+		};
 
-     				if (zoomlevel > 0 && zoomlevel < 4) {
+		image.addMouseListener(adapter);
+		image.addMouseMotionListener(adapter);
 
-     					if (zoomlevel == 3) {
-     						new FirstZoomLevel();
-     						image.setURI(new File("singapore.svg").toURI().toString());
-     					} else if (zoomlevel == 2) {
-     						new SecondZoomLevel();
-     						image.setURI(new File("singapore.svg").toURI().toString());
-     					} else if (zoomlevel == 1) {
-     						new ThirdZoomLevel();
-     						image.setURI(new File("singapore.svg").toURI().toString());
-     					}
-     					action.actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, null));
-     				} else if (zoomlevel <= 0) {
-     					setZoomlevel(1);
-     				} else {
-     					setZoomlevel(3);
-     				}
-     			}
-			 };
-
-		map.addMouseListener(adapter);
-        map.addMouseMotionListener(adapter);
-        image.addMouseWheelListener(adapter);
-        
 		suggestion.addItem("Quickest");
 		suggestion.addItem("Cheapest");
 		suggestion.addItem("Touristic");
 		summary.setText("Summary");
 		summary.setText("Summary");
-		map.setToolTipText("Map");
+		image.setToolTipText("Map");
 
 		JSplitPane sumSug = new JSplitPane(JSplitPane.VERTICAL_SPLIT, suggestion, summary);
-		JSplitPane splitMap = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, sumSug, map);
+
+		JSplitPane splitMap = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, sumSug, image);
 		splitMap.setDividerLocation(250);
 		sumSug.setDividerLocation(35);
+
 		return splitMap;
 	}
-
-	public static int getZoomlevel() {
-		return zoomlevel;
-	}
-
-	public static void setZoomlevel(int zoomlevel) {
-		SuggestionSummary.zoomlevel = zoomlevel;
-	}
-
 }
