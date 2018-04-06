42684f04510485f1064e861b9adbe313f146a73a
Anlodar <35761452+Anlodar@users.noreply.github.com>
2018-04-06 17:19:04 +0200

    Add files via upload

---
+++ src/parsing/CSVDocument.java
@@ -0,0 +1,24 @@
+package parsing;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class CSVDocument {
+	private HashMap<String, String> nodes = new HashMap<String, String>();
+	
+	public CSVDocument(String file) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		String line;
+		while ((line = reader.readLine()) != null) {
+			if(!nodes.containsKey(line.substring(0, line.indexOf("::"))))
+				nodes.put(line.substring(0, line.indexOf("::")), line.substring(line.indexOf("::") + 2));
+		}
+		reader.close();
+	}
+
+	public HashMap<String, String> getNodes() {
+		return nodes;
+	}
+}
